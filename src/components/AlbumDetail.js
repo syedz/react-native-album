1d976fa4b94de27f5b85f3d626d4030ccc3cd3a4
Zeeshan Syed <zsyed89@gmail.com>
2019-03-26 23:51:16 -0400

    Created button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
 
     return (
@@ -20,6 +21,9 @@ const AlbumDetail = ({ album }) => {
             </CardSection>
             <CardSection>
                 <Image style={imageStyle} source={{ uri: image }} />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
             </CardSection>
         </Card>
     );
